b2fbb87c2d97362714c8f2dd86390ef684155e08
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-04-22 21:40:46 -0700

    Added better darkmode button

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const toggle = document.querySelector('#color-theme');
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark');
+  toggle.textContent = 'Dark Mode';
 }
 
 toggle.addEventListener('click', () => {
   const current = root.getAttribute('data-theme');
   const next = current === 'dark' ? 'light' : 'dark';
   root.setAttribute('data-theme', next);
+  toggle.textContent = next === 'dark' ? 'Dark Mode' : 'Light Mode';
 });
